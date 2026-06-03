23e226ef89b6f1e18f75ef0b4831753d79ab3bb6
Angus Hollands <goosey15@gmail.com>
2026-06-03 13:47:49 +0100

    wip: initial dev commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+# code-owner: @agoose77
+# This flake sets up a dev-shell that installs all the required
+# packages for running deployer, and then installs the tool in the virtual environment
+# It is not best-practice for the nix-way of distributing this code,
+# but its purpose is to get an environment up and running.
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in {
+      default = pkgs.mkShell {
+        packages = with pkgs; [minijinja];
+      };
+    });
+  };
+}
